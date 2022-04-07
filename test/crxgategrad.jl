fc3db5a97cef66148ebd39706e623d98b68b630a
guo chu <guochu604b@gmail.com>
2022-04-07 09:12:32 +0800

    add some test

--- test/crxgategrad.jl
+++ test/crxgategrad.jl
@@ -34,6 +34,101 @@ function crx_circuit_grad_dot_real(L::Int, depth::Int)
 	return nparameters(circuit)==n && maximum(abs.(grad1 - grad2)) < 1.0e-6
 end
 
+
+function gfsim1_circuit(L::Int, depth::Int)
+	circuit = QCircuit()
+	for i in 1:L
+	    push!(circuit, HGate(i))
+	end
+	for d in 1:depth
+	   	for i in 1:L-1
+	   		push!(circuit, CNOTGate((i, i+1)))
+	   		push!(circuit, FSIMGate((i, i+1), randn(5), isparas=[true, false, false, false, false]))
+	   		push!(circuit, CNOTGate((i, i+1)))
+		end 
+	end
+	return circuit
+end
+
+function gfsim2_circuit(L::Int, depth::Int)
+	circuit = QCircuit()
+	for i in 1:L
+	    push!(circuit, HGate(i))
+	end
+	for d in 1:depth
+	   	for i in 1:L-1
+	   		push!(circuit, CNOTGate((i, i+1)))
+	   		push!(circuit, FSIMGate((i, i+1), randn(5), isparas=[true, true, false, false, false]))
+	   		push!(circuit, CNOTGate((i, i+1)))
+		end 
+	end
+	return circuit
+end
+
+function gfsim5_circuit(L::Int, depth::Int)
+	circuit = QCircuit()
+	for i in 1:L
+	    push!(circuit, HGate(i))
+	end
+	for d in 1:depth
+	   	for i in 1:L-1
+	   		push!(circuit, CNOTGate((i, i+1)))
+	   		push!(circuit, FSIMGate((i, i+1), randn(5), isparas=[true, true, true, true, true]))
+	   		push!(circuit, CNOTGate((i, i+1)))
+		end 
+	end
+	return circuit
+end
+
+function bf_circuit_gfims1_grad_dot_abs(L::Int, depth::Int)
+	target_state = rand_state(ComplexF64, L)
+	initial_state = StateVector(ComplexF64, L)
+	circuit =  gfsim1_circuit(L, depth)
+
+	loss(x) = abs(dot(target_state, x * initial_state))
+	loss_fd(θs) = begin
+		reset_parameters!(circuit, θs)
+		return loss(circuit)
+	end
+
+	grad1 = gradient(loss, circuit)[1]
+	grad2 = fdm_gradient(loss_fd, active_parameters(circuit))
+	return maximum(abs.(grad1 - grad2)) < 1.0e-6
+end
+
+function bf_circuit_gfims2_grad_dot_abs(L::Int, depth::Int)
+	target_state = rand_state(ComplexF64, L)
+	initial_state = StateVector(ComplexF64, L)
+	circuit =  gfsim2_circuit(L, depth)
+
+	loss(x) = abs(dot(target_state, x * initial_state))
+	loss_fd(θs) = begin
+		reset_parameters!(circuit, θs)
+		return loss(circuit)
+	end
+
+	grad1 = gradient(loss, circuit)[1]
+	grad2 = fdm_gradient(loss_fd, active_parameters(circuit))
+	return maximum(abs.(grad1 - grad2)) < 1.0e-6
+end
+
+function bf_circuit_gfims5_grad_dot_abs(L::Int, depth::Int)
+	target_state = rand_state(ComplexF64, L)
+	initial_state = StateVector(ComplexF64, L)
+	circuit =  gfsim5_circuit(L, depth)
+
+	loss(x) = abs(dot(target_state, x * initial_state))
+	loss_fd(θs) = begin
+		reset_parameters!(circuit, θs)
+		return loss(circuit)
+	end
+
+	grad1 = gradient(loss, circuit)[1]
+	grad2 = fdm_gradient(loss_fd, active_parameters(circuit))
+	return maximum(abs.(grad1 - grad2)) < 1.0e-6
+end
+
+
 @testset "gradient of variable quantum circuit with parameterized controled rotational gate" begin
 	for L in 2:5
 		for depth in 1:5
@@ -41,3 +136,14 @@ end
 		end	    
 	end
 end
+
+@testset "gradient of brute force quantum circuit with FSIM gate" begin
+	for L in 2:5
+		for depth in 1:3
+		    @test bf_circuit_gfims1_grad_dot_abs(L, depth)
+		    @test bf_circuit_gfims2_grad_dot_abs(L, depth)
+		    @test bf_circuit_gfims5_grad_dot_abs(L, depth)
+		end
+	end
+end
+
