e216cb0d53d42f85178054eba202122dd01afc9c
guo chu <guochu604b@gmail.com>
2022-04-10 10:36:36 +0800

    remove variational circuit 2d

--- src/utility/utility.jl
+++ src/utility/utility.jl
@@ -5,7 +5,7 @@ using QuantumCircuits, QuantumCircuits.Gates
 
 export heisenberg_1d, heisenberg_2d, ising_1d, ising_2d, ground_state, time_evolution
 export QFT
-export variational_circuit, variational_circuit_1d, variational_circuit_2d, real_variational_circuit_1d, real_variational_circuit_2d
+export variational_circuit_1d, real_variational_circuit_1d
 export order_finding
 
 include("spin_hamiltonians.jl")
--- src/utility/variationalcircuit.jl
+++ src/utility/variationalcircuit.jl
@@ -16,8 +16,14 @@ function variational_circuit_1d(L::Int, depth::Int; θs::Vector{<:Real}=rand(_np
 		ncount += 1
 	end		
 	for i in 1:depth
-		for j in 1:(L-1)
-		    push!(circuit, CNOTGate(j, j+1))
+		if isodd(i)
+			for j in 1:(L-1)
+		    	push!(circuit, CNOTGate(j, j+1))
+			end
+		else
+			for j in (L-1):-1:1
+		    	push!(circuit, CNOTGate(j, j+1))
+			end			
 		end
 		for j in 1:L
 			push!(circuit, RzGate(j, paras[ncount], isparas=true))
@@ -42,8 +48,14 @@ function real_variational_circuit_1d(L::Int, depth::Int; θs::Vector{<:Real}=ran
 		ncount += 1
 	end		
 	for i in 1:depth
-		for j in 1:(L-1)
-		    push!(circuit, CNOTGate(j, j+1))
+		if isodd(i)
+			for j in 1:(L-1)
+		    	push!(circuit, CNOTGate(j, j+1))
+			end
+		else
+			for j in (L-1):-1:1
+		    	push!(circuit, CNOTGate(j, j+1))
+			end			
 		end
 		for j in 1:L
 			push!(circuit, RyGate(j, paras[ncount], isparas=true))
@@ -55,79 +67,79 @@ function real_variational_circuit_1d(L::Int, depth::Int; θs::Vector{<:Real}=ran
 end
 
 
-function variational_circuit_2d(m::Int, n::Int, depth::Int; θs::Vector{<:Real}=rand(_nparas(ComplexF64, m*n, depth)) .* 2π)
-	paras = θs
-	L = m*n
-	(length(paras) == _nparas(ComplexF64, L, depth)) || throw("wrong number of parameters.")
-	circuit = QCircuit()
-	ncount = 1
-	for i in 1:L
-		push!(circuit, RzGate(i, paras[ncount], isparas=true))
-		ncount += 1
-		push!(circuit, RyGate(i, paras[ncount], isparas=true))
-		ncount += 1
-		push!(circuit, RzGate(i, paras[ncount], isparas=true))
-		ncount += 1
-	end	
+# function variational_circuit_2d(m::Int, n::Int, depth::Int; θs::Vector{<:Real}=rand(_nparas(ComplexF64, m*n, depth)) .* 2π)
+# 	paras = θs
+# 	L = m*n
+# 	(length(paras) == _nparas(ComplexF64, L, depth)) || throw("wrong number of parameters.")
+# 	circuit = QCircuit()
+# 	ncount = 1
+# 	for i in 1:L
+# 		push!(circuit, RzGate(i, paras[ncount], isparas=true))
+# 		ncount += 1
+# 		push!(circuit, RyGate(i, paras[ncount], isparas=true))
+# 		ncount += 1
+# 		push!(circuit, RzGate(i, paras[ncount], isparas=true))
+# 		ncount += 1
+# 	end	
 
-	index = LinearIndices((m, n))
-	for l in 1:depth
-		for i in 1:m
-		    for j in 1:(n-1)
-		        push!(circuit, CNOTGate(index[i, j], index[i, j+1]))
-		    end
-		end
-		for i in 1:(m-1)
-		    for j in 1:n
-		        push!(circuit, CNOTGate(index[i, j], index[i+1, j]))
-		    end
-		end
-		for i in 1:L
-			push!(circuit, RzGate(i, paras[ncount], isparas=true))
-			ncount += 1
-			push!(circuit, RyGate(i, paras[ncount], isparas=true))
-			ncount += 1
-			push!(circuit, RzGate(i, paras[ncount], isparas=true))
-			ncount += 1
-		end	
-	end
-	@assert ncount == length(paras)+1
-	return circuit
-end
-variational_circuit_2d(shapes::Tuple{Int, Int}, args...; kwargs...) = variational_circuit_2d(shapes[1], shapes[2], args...; kwargs...)
+# 	index = LinearIndices((m, n))
+# 	for l in 1:depth
+# 		for i in 1:m
+# 		    for j in 1:(n-1)
+# 		        push!(circuit, CNOTGate(index[i, j], index[i, j+1]))
+# 		    end
+# 		end
+# 		for i in 1:(m-1)
+# 		    for j in 1:n
+# 		        push!(circuit, CNOTGate(index[i, j], index[i+1, j]))
+# 		    end
+# 		end
+# 		for i in 1:L
+# 			push!(circuit, RzGate(i, paras[ncount], isparas=true))
+# 			ncount += 1
+# 			push!(circuit, RyGate(i, paras[ncount], isparas=true))
+# 			ncount += 1
+# 			push!(circuit, RzGate(i, paras[ncount], isparas=true))
+# 			ncount += 1
+# 		end	
+# 	end
+# 	@assert ncount == length(paras)+1
+# 	return circuit
+# end
+# variational_circuit_2d(shapes::Tuple{Int, Int}, args...; kwargs...) = variational_circuit_2d(shapes[1], shapes[2], args...; kwargs...)
 
-function real_variational_circuit_2d(m::Int, n::Int, depth::Int; θs::Vector{<:Real}=rand(_nparas(Float64, m*n, depth)) .* 2π)
-	paras = θs
-	L = m*n
-	(length(paras) == _nparas(Float64, L, depth)) || throw("wrong number of parameters.")
-	circuit = QCircuit()
-	ncount = 1
-	for i in 1:L
-		push!(circuit, RyGate(i, paras[ncount], isparas=true))
-		ncount += 1
-	end			
+# function real_variational_circuit_2d(m::Int, n::Int, depth::Int; θs::Vector{<:Real}=rand(_nparas(Float64, m*n, depth)) .* 2π)
+# 	paras = θs
+# 	L = m*n
+# 	(length(paras) == _nparas(Float64, L, depth)) || throw("wrong number of parameters.")
+# 	circuit = QCircuit()
+# 	ncount = 1
+# 	for i in 1:L
+# 		push!(circuit, RyGate(i, paras[ncount], isparas=true))
+# 		ncount += 1
+# 	end			
 
-	index = LinearIndices((m, n))
-	for l in 1:depth
-		for i in 1:m
-		    for j in 1:(n-1)
-		        push!(circuit, CNOTGate(index[i, j], index[i, j+1]))
-		    end
-		end
-		for i in 1:(m-1)
-		    for j in 1:n
-		        push!(circuit, CNOTGate(index[i, j], index[i+1, j]))
-		    end
-		end
-		for i in 1:L
-			push!(circuit, RyGate(i, paras[ncount], isparas=true))
-			ncount += 1
-		end	
-	end
-	@assert ncount == length(paras)+1
-	return circuit
-end
-real_variational_circuit_2d(shapes::Tuple{Int, Int}, args...; kwargs...) = real_variational_circuit_2d(shapes[1], shapes[2], args...; kwargs...)
+# 	index = LinearIndices((m, n))
+# 	for l in 1:depth
+# 		for i in 1:m
+# 		    for j in 1:(n-1)
+# 		        push!(circuit, CNOTGate(index[i, j], index[i, j+1]))
+# 		    end
+# 		end
+# 		for i in 1:(m-1)
+# 		    for j in 1:n
+# 		        push!(circuit, CNOTGate(index[i, j], index[i+1, j]))
+# 		    end
+# 		end
+# 		for i in 1:L
+# 			push!(circuit, RyGate(i, paras[ncount], isparas=true))
+# 			ncount += 1
+# 		end	
+# 	end
+# 	@assert ncount == length(paras)+1
+# 	return circuit
+# end
+# real_variational_circuit_2d(shapes::Tuple{Int, Int}, args...; kwargs...) = real_variational_circuit_2d(shapes[1], shapes[2], args...; kwargs...)
 
 function _nparas(::Type{T}, L::Int, depth::Int) where {T <: Number} 
 	n = depth+1
